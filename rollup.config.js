1c7f956431e01a4942630c94b3251bf9bd3bc573
Akram <akramm@moblize.com>
2023-01-07 22:44:49 +0530

    type defnition issue resolved

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import sass from 'rollup-plugin-sass'
 import typescript from 'rollup-plugin-typescript2'
 import external from 'rollup-plugin-peer-deps-external'
 import pkg from './package.json' assert { type: 'json' }
-import dts from 'rollup-plugin-dts'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import terser from '@rollup/plugin-terser'
@@ -32,12 +31,6 @@ export default {
     sass({ insert: true }),
     typescript({ objectHashIgnoreUnknownHack: false }),
     terser(),
-    {
-      input: 'dist/esm/types/index.d.ts',
-      output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-      external: [/\.scss$/],
-      plugins: [dts()],
-    },
   ],
 
   external: ['react', 'react-dom'],
